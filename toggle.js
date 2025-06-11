f5035a9fdd7f7aec95a6ee69f2e0a08803d86ac1
karthik-B-byte <buradak147@gmail.com>
2025-06-11 13:59:48 +0100

    Delete toggle.js

--- toggle.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.product h3').forEach(title => {
-  title.addEventListener('click', () => {
-    const specs = title.nextElementSibling;
-    if (specs && specs.tagName === 'UL') {
-      specs.classList.toggle('show');
-      title.classList.toggle('active');
-    }
-  });
-});
